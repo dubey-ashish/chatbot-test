efbf096cda25082ac5954e792dc00fa97553c6d3
Ashish Dubey <ashish@Ashishs-MacBook-Air.local>
2026-01-19 10:00:59 +0530

    comments

--- src/main/java/com/open/chatbottest/ChatbottestApplication.java
+++ src/main/java/com/open/chatbottest/ChatbottestApplication.java
@@ -1,32 +1,30 @@
 package com.open.chatbottest;
 
+import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 
-import java.util.Arrays;
+
 import java.util.Map;
 import java.util.TreeMap;
 
 @SpringBootApplication
 public class ChatbottestApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ChatbottestApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ChatbottestApplication.class, args);
+    }
 
-	@Bean
-	ApplicationRunner beanDebugger(ApplicationContext context) {
-		ApplicationRunner applicationRunner = args -> {
-			Map<String, Object> beans = new TreeMap<>(context.getBeansOfType(Object.class));
-			// 🔴 PUT BREAKPOINT HERE
-			System.out.println("Total beans: " + beans.size());
-		};
-		return applicationRunner;
-	}
-
-
-
+    @Bean
+    ApplicationRunner beanDebugger(ApplicationContext context) {
+        ApplicationRunner applicationRunner = args -> {
+            Map<String, Object> beans = new TreeMap<>(context.getBeansOfType(Object.class));
+            // 🔴 PUT BREAKPOINT HERE
+            System.out.println("Total beans: " + beans.size());
+        };
+        return applicationRunner;
+    }
 }
--- src/main/java/com/open/chatbottest/controller/LlmController.java
+++ src/main/java/com/open/chatbottest/controller/LlmController.java
@@ -7,6 +7,7 @@ import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
 import org.springframework.ai.chat.memory.ChatMemory;
 import org.springframework.ai.chat.memory.MessageWindowChatMemory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -24,12 +25,15 @@ public class LlmController {
     private final ChatMemory chatMemory;
 
     // You initialize the ChatClient with the foundation of communication. Things like temp, models, tokens can be specified here
+    // @Autowired is not needed anymore for newer versions of Spring to inject Bean to ChatClient.Builder chatClientBuilder; a bean which is already created in ChatClientAutoConfiguration class.
+    // when you use builder(s) the same object is passed successively to next. Using Builder we kinda create and populate a temporary object and then use that object to populate the fields of main class's object
+    // whenever you finally use the .build() its not the object of the static Builder class that gets build but the main class's object
     public LlmController(ChatClient.Builder chatClientBuilder, ChatMemory chatMemory) {
-        this.chatMemory = MessageWindowChatMemory.builder().build();
+        this.chatMemory = MessageWindowChatMemory.builder().maxMessages(10).build();
         this.chatClient = chatClientBuilder
                 .defaultAdvisors(List.of(new SimpleLoggerAdvisor(), MessageChatMemoryAdvisor.builder(chatMemory).build()))
                 .defaultSystem("You are a considerate friend")
-                .build();
+                .build();   // returns DefaultChatClient.class object
     }
 
     private static final Logger log =
@@ -51,10 +55,11 @@ public class LlmController {
     ResponseEntity<String> generation(
             @RequestParam("userInput") String userInput,
             @RequestParam("conversationId") String conversationId) {
-        return ResponseEntity.ok(this.chatClient.prompt()
+        return ResponseEntity.ok(
+                this.chatClient.prompt()
                 .advisors(a -> a.param(ChatMemory.CONVERSATION_ID, conversationId))
                 .user(userInput)
                 .call()
-                .content());
+                .content());    // this calls result generating code block
     }
 }
